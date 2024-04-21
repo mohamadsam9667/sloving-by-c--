f7738528d0dfa532950337eb06e1aa325ba8f76d
mohamadsam9667 <146078669+mohamadsam9667@users.noreply.github.com>
2024-04-21 13:37:56 +0330

    program
    input--------------output
    1-------------------wow!
    2------------------woow!
    3-----------------woooow!

--- questions-yellow.cpp
+++ questions-yellow.cpp
@@ -1,13 +1,23 @@
 #include <iostream>
-#include <conio.h>
 
 using namespace std;
 
 int main(){
-    for(int i=0;i<=10;i++)
+    char question;
+     cout<<"you want continue?(y/n)";
+    cin>>question;
+    while(question=='y')
     {
-        cout<<"sam\t";
-        setw(10);
+    int n=2;
+    cin>>n;
+    cout<<"W";
+    for(int i=1; i<=n;i++)
+    {
+        cout<<"o";
+    }
+    cout<<"w!";
+     cout<<"you want continue?(y/n)";
+    cin>>question;
     }
     return 0;
 
