58a83c6f5e8b154eaeeee0b3d6e6fb3da2071a7d
mohamadsam9667 <146078669+mohamadsam9667@users.noreply.github.com>
2024-04-21 12:39:04 +0330

    start file c++

---
+++ questions-yellow.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main(){
+    for(int i=0;i<=10;i++)
+    {
+        cout<<"sam\t";
+        setw(10);
+    }
+    return 0;
+
+}
