8f6703a9a33e9bea34392a94f06d57a07631db13
mohamadsam9667 <146078669+mohamadsam9667@users.noreply.github.com>
2024-05-03 16:03:04 +0330

    sort

--- questions-yellow.cpp
+++ questions-yellow.cpp
@@ -1,24 +1,45 @@
 #include <iostream>
 
 using namespace std;
+void quick_sort(int A[] ,int low ,int high){
+    if (low>=high)
+    return;
+    else 
+    {
+        j=partion(A,low,high);
+        quick_sort(A[],low,j-1);
+        quick_sort(A[],j+1,high);
+        combaine not requested;
+    }
+
+}
+int partion(int A[],int low,int high)
+{
+    v=A[low];i=low;j=high+1;
+    do{
+        do{
+            i++;
+        }while(A(i)<v)
+        do{
+            j--;
+        }while(A(j)>v)
+        if(i<j) exchange (A,i,j);
+    }while(i<=j)
+    A(low)=A(j);
+    A(j)=v;
+    return j;
+
+
+
+}
 
 int main(){
-    char question;
-     cout<<"you want continue?(y/n)";
-    cin>>question;
-    while(question=='y')
-    {
-    int n=2;
-    cin>>n;
-    cout<<"s";
-    for(int i=1; i<=n;i++)
-    {
-        cout<<"a";
-    }
-    cout<<"m!"<<endl;
-     cout<<"you want continue?(y/n)";
-    cin>>question;
-    }
+
+
+
+
+
+
     return 0;
 
 }
